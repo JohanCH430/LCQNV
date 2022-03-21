384b5b872ea78a7d5e90c9c020ad52da8e24df84
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-03-21 16:49:01 +0100

    Update TextoBll.java

--- LCQNV/app/src/main/java/com/example/lascosasquenovemos/bll/TextoBll.java
+++ LCQNV/app/src/main/java/com/example/lascosasquenovemos/bll/TextoBll.java
@@ -1,5 +1,14 @@
 package com.example.lascosasquenovemos.bll;
 
+import com.example.lascosasquenovemos.dal.TextoDAL;
+
 public class TextoBll {
-    
+
+    private TextoDAL textoDAL;
+
+    public int crearTexto(){
+        //TODO depende de los Transfers
+        return textoDAL.crearTexto();
+    }
+
 }
