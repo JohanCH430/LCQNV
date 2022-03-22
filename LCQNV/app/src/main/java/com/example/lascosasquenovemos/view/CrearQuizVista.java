67f1d4b1b4824d66160d8dd3ad931e2302600fb9
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-03-22 11:38:35 +0100

    Creación de clase Vista Quiz.

---
+++ LCQNV/app/src/main/java/com/example/lascosasquenovemos/view/CrearQuizVista.java
@@ -0,0 +1,14 @@
+package com.example.lascosasquenovemos.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearQuizVista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_quiz_vista);
+    }
+}
