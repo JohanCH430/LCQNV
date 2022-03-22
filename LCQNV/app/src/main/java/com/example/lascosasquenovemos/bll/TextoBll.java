975052bb8cdacaaa17c6069cbfe98b72ee7e3264
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-03-22 11:07:02 +0100

    Pequeño cambio de método

--- LCQNV/app/src/main/java/com/example/lascosasquenovemos/bll/TextoBll.java
+++ LCQNV/app/src/main/java/com/example/lascosasquenovemos/bll/TextoBll.java
@@ -1,14 +1,15 @@
 package com.example.lascosasquenovemos.bll;
 
 import com.example.lascosasquenovemos.dal.TextoDAL;
+import com.example.lascosasquenovemos.model.TextoModelo;
 
 public class TextoBll {
 
     private TextoDAL textoDAL;
 
-    public int crearTexto(){
+    public int crearTexto(TextoModelo texto){
         //TODO depende de los Transfers
-        return textoDAL.crearTexto();
+        //return textoDAL.crearTexto(texto);
     }
 
 }
