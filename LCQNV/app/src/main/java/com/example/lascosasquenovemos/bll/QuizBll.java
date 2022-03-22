fa848b705039ceeb567d2204d37c660948435c86
kun-sys <73662477+kun-sys@users.noreply.github.com>
2022-03-22 21:09:31 +0100

    VistaTexto

---
+++ LCQNV/app/src/main/java/com/example/lascosasquenovemos/bll/QuizBll.java
@@ -0,0 +1,17 @@
+package com.example.lascosasquenovemos.bll;
+
+import com.example.lascosasquenovemos.dal.TextoDAL;
+import com.example.lascosasquenovemos.model.QuizModelo;
+
+public class QuizBll {
+    private QuizDAL integracion;
+
+    public QuizBll(){
+
+    }
+    private boolean crearQuiz(QuizModelo quiz){
+        //Comprobar el sintaxis
+        //Si es correcto entonces podemos seguir sino devuelve un false para la pantalla quizVista
+
+    }
+}
