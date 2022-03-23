e558c0ec7da94dd87560b9527a188daad32a6a14
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-03-23 21:51:10 +0100

    Terminado el QuizDAL.
    Cualquier cosa que falte, se añadirá en el futuro.

--- LCQNV/app/src/main/java/com/example/lascosasquenovemos/dal/QuizDAL.java
+++ LCQNV/app/src/main/java/com/example/lascosasquenovemos/dal/QuizDAL.java
@@ -30,8 +30,8 @@ public class QuizDAL extends FirebaseDAL {
                 } else {
                     Log.d("firebase", String.valueOf(task.getResult().getValue()));
 
-                    if (String.valueOf(task.getResult().getValue()) == "null") {
-
+                    if (String.valueOf(task.getResult().getValue()) == "null") { //Cuando falla la busqueda en la BD, porque no existe ningún Quiz con esa id, te devuelve un null.
+                        qL.onQuizReadSucced(null);
                     } else {
 
                         //Java interpreta lo recibido como un HashMap, unicamente hay que parsearlo por claves.
@@ -62,7 +62,7 @@ public class QuizDAL extends FirebaseDAL {
 
         DatabaseReference refQuizID = FirebaseDAL.dataBase.child("IDQuiz");
         DatabaseReference refQuiz = FirebaseDAL.dataBase.child("Quiz");
-        DatabaseReference refTextoQuiz = FirebaseDAL.dataBase.child("Quiz");
+        DatabaseReference refTextoQuiz = FirebaseDAL.dataBase.child("TextoPregunta");
 
         refQuizID.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -109,8 +109,24 @@ public class QuizDAL extends FirebaseDAL {
                         }
                     });
 
-                    //TODO Añadir relación texto-pregunta Preguntar
+                    //TODO Añadir relación texto-pregunta Preguntar Sujeto a cambio
 
+                    //Añado la relación entre Texto y pregunta. (Un texto, muchas preguntas) (Tiene que existir la referencia en la BD, no te la autocrea)
+                    refTextoQuiz.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            HashMap<String, String> resultTextoPregunta = (HashMap<String, String>) task.getResult().getValue();
+
+                            if(resultTextoPregunta.get(quiz.getTextId()) == null){
+                                refTextoQuiz.child(quiz.getTextId()).setValue(nuevoID);
+                            }else{
+                                String suma = resultTextoPregunta.get(quiz.getTextId());
+                                suma += ", " + nuevoID;
+                                refTextoQuiz.child(quiz.getTextId()).setValue(suma);
+                            }
+
+                        }
+                    });
 
                 }
 
