3b91d2e5cb6ae67507e2db7ffdf1091ab60ce59d
inirolga <100686766+InigoRolando@users.noreply.github.com>
2022-04-19 10:02:32 +0200

    EntrarEnPartidaVista
    Se ha hecho la interfaz y la gestion de errores pero falta la redirección

---
+++ app/src/main/java/com/example/lascosasquenovemos/view/EntrarEnPartidaVista.java
@@ -0,0 +1,52 @@
+package com.example.lascosasquenovemos.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.lascosasquenovemos.bll.PartidaBll;
+import com.example.lascosasquenovemos.model.Interfaces.PartidaListener;
+import com.example.lascosasquenovemos.model.PartidaModelo;
+
+public class EntrarEnPartidaVista extends AppCompatActivity implements PartidaListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrar_en_partida_vista);
+
+        Button bContinuar = findViewById(R.id.botonJugar);
+        EditText editText = findViewById(R.id.EditTextCodigo);
+
+        bContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String codigo = editText.getText().toString();
+                if (codigo.equals(null) || codigo.trim().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "El código introducido es vacío", Toast.LENGTH_LONG).show();
+                }
+
+                else PartidaBll.leerPartida(codigo, EntrarEnPartidaVista.this);
+            }
+        });
+    }
+
+    @Override
+    public void onPartidaReadSuccess(PartidaModelo pM) {
+        if (pM != null){
+            //TODO pasar a la siguiente pantalla
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Error al leer la partida, no se ha encontrado ninguna partida con el código introducido", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPartidaWriteSuccess(Boolean correct) {
+
+    }
+}
