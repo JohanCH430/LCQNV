67d6bf29f6b1fcd1e981616ddf134bf396c97225
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-04-24 15:05:55 +0200

    Test de Integración (Empezados) de entrar en partida con código.

--- app/src/main/java/com/example/lascosasquenovemos/view/EntrarEnPartidaVista.java
+++ app/src/main/java/com/example/lascosasquenovemos/view/EntrarEnPartidaVista.java
@@ -7,28 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lascosasquenovemos.bll.PartidaBll;
+import com.example.lascosasquenovemos.dal.FirebaseDAL;
 import com.example.lascosasquenovemos.model.Interfaces.PartidaListener;
 import com.example.lascosasquenovemos.model.PartidaModelo;
 
 public class EntrarEnPartidaVista extends AppCompatActivity implements PartidaListener {
+
+    TextView fedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrar_en_partida_vista);
 
+        FirebaseDAL.getInstance(getApplicationContext());
+
         Button bContinuar = findViewById(R.id.botonJugar);
         EditText editText = findViewById(R.id.EditTextCodigo);
+        fedback = findViewById(R.id.TextViewFedBackIntroducirCodigo);
 
         bContinuar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String codigo = editText.getText().toString();
                 if (!PartidaBll.comprobarSintaxisCodigoParitda(codigo)){
-                    Toast.makeText(getApplicationContext(), "El código introducido es vacío", Toast.LENGTH_LONG).show();
+                    fedback.setText("Error, el código introducido es vacío");
                 }
                 else PartidaBll.leerPartida(codigo, EntrarEnPartidaVista.this);
             }
@@ -43,7 +50,7 @@ public class EntrarEnPartidaVista extends AppCompatActivity implements PartidaLi
             startActivity(iInfoVista);
         }
         else {
-            Toast.makeText(getApplicationContext(), "Error al leer la partida, no se ha encontrado ninguna partida con el código introducido", Toast.LENGTH_LONG).show();
+            fedback.setText("Error al leer la partida, el código introducido no existe");
         }
     }
 
