5f1652e392027cbef1ab29bd3ed953e1f5cb84ab
inirolga <100686766+InigoRolando@users.noreply.github.com>
2022-04-25 17:19:48 +0200

    Actualizar previsualizar vista

--- app/src/main/java/com/example/lascosasquenovemos/view/VerPartidaVista.java
+++ app/src/main/java/com/example/lascosasquenovemos/view/VerPartidaVista.java
@@ -12,45 +12,49 @@ import com.example.lascosasquenovemos.model.PartidaModelo;
 
 public class VerPartidaVista extends AppCompatActivity {
 
+    private Button btnAnterior, btnTerminar, btnContinuar;
+    private TextView textNumPantalla,textInfo,textPreg, textOpc1, textOpc2, textOpc3, textOpc4;
+    PartidaModelo partidaModelo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_partida_vista);
 
-        Button btnAnterior = findViewById(R.id.BtnAnteriorVerPtll);
-        Button btnTerminar = findViewById(R.id.BtnTerminarVerPtll);
-        Button btnContinuar = findViewById(R.id.BtnSiguienteVerPtll);
+        //Inicializar variables para poder añadir los listeenrs de los botones y cambiar los textos
+        btnAnterior = findViewById(R.id.BtnAnteriorVerPtll);
+        btnTerminar = findViewById(R.id.BtnTerminarVerPtll);
+        btnContinuar = findViewById(R.id.BtnSiguienteVerPtll);
 
-        TextView textInfo = findViewById(R.id.TextInfoVerPartida);
-        TextView textPreg = findViewById(R.id.TextPregVerPartida);
-        TextView textOpc1 = findViewById(R.id.TextOpc1VerPartida);
-        TextView textOpc2 = findViewById(R.id.TextOpc2VerPartida);
-        TextView textOpc3 = findViewById(R.id.TextOpc3VerPartida);
-        TextView textOpc4 = findViewById(R.id.TextOpc4VerPartida);
+        textNumPantalla = findViewById(R.id.InfoNumPantallas);
+        textInfo = findViewById(R.id.TextInfoVerPartida);
+        textPreg = findViewById(R.id.TextPregVerPartida);
+        textOpc1 = findViewById(R.id.TextOpc1VerPartida);
+        textOpc2 = findViewById(R.id.TextOpc2VerPartida);
+        textOpc3 = findViewById(R.id.TextOpc3VerPartida);
+        textOpc4 = findViewById(R.id.TextOpc4VerPartida);
 
-        PartidaModelo partidaModelo = (PartidaModelo) getIntent().getSerializableExtra("PARTIDA");
+        //Se obtiene la partida que ha llegado de la pantalla anterior
+        partidaModelo = (PartidaModelo) getIntent().getSerializableExtra("PARTIDA");
+
         final int[] indice = {0};
         final int[] maxPant = {partidaModelo.getNumPantallas() - 1};
         PantallaModelo pantalla = (PantallaModelo) partidaModelo.getPantallasPartida().values().toArray()[indice[0]];
-        textInfo.setText(pantalla.getTexto().getTexto());
-        textPreg.setText(pantalla.getQuiz().getPregunta());
-        textOpc1.setText(pantalla.getQuiz().getOpcionA());
-        textOpc2.setText(pantalla.getQuiz().getOpcionB());
-        textOpc3.setText(pantalla.getQuiz().getOpcionC());
-        textOpc4.setText(pantalla.getQuiz().getOpcionD());
+
+        changeText(0, maxPant[0]);
 
         btnAnterior.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (indice[0] > 0) {
+                    //En caso de que no sea la primera pantalla se resta uno al indice y se muestra la siguiente pantalla
                     indice[0]--;
-                    PantallaModelo pantalla = (PantallaModelo) partidaModelo.getPantallasPartida().values().toArray()[indice[0]];
-                    textInfo.setText(pantalla.getTexto().getTexto());
-                    textPreg.setText(pantalla.getQuiz().getPregunta());
-                    textOpc1.setText(pantalla.getQuiz().getOpcionA());
-                    textOpc2.setText(pantalla.getQuiz().getOpcionB());
-                    textOpc3.setText(pantalla.getQuiz().getOpcionC());
-                    textOpc4.setText(pantalla.getQuiz().getOpcionD());
+                    changeText(indice[0], maxPant[0]);
+                }
+                else {
+                    //En caso contrario la pantalla anterior seria la ultima pantalla
+                    indice[0] = maxPant[0];
+                    changeText(indice[0], maxPant[0]);
                 }
             }
         });
@@ -58,6 +62,7 @@ public class VerPartidaVista extends AppCompatActivity {
         btnTerminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Cuando se termine de previsualizar se termina este activity y se vuelve a la pantalla anterior
                 finish();
             }
         });
@@ -66,16 +71,48 @@ public class VerPartidaVista extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (indice[0] < maxPant[0]) {
+                    //Si no es la ultima pantalla se suma uno al indice y se muestra la siguiente pantalla
                     indice[0]++;
-                    PantallaModelo pantalla = (PantallaModelo) partidaModelo.getPantallasPartida().values().toArray()[indice[0]];
-                    textInfo.setText(pantalla.getTexto().getTexto());
-                    textPreg.setText(pantalla.getQuiz().getPregunta());
-                    textOpc1.setText(pantalla.getQuiz().getOpcionA());
-                    textOpc2.setText(pantalla.getQuiz().getOpcionB());
-                    textOpc3.setText(pantalla.getQuiz().getOpcionC());
-                    textOpc4.setText(pantalla.getQuiz().getOpcionD());
+                    changeText(indice[0], maxPant[0]);
+                }
+                else {
+                    //En caso contrario la pantalla seria la primera
+                    indice[0] = 0;
+                    changeText(indice[0], maxPant[0]);
                 }
             }
         });
+    }
+
+    private void changeText(int indice, int maxPant){
+        int aux1 = indice +1;
+        int aux2 = maxPant+1;
+        //Texto para mostrar el numero de la pantalla en la que te encuentras
+        textNumPantalla.setText("Previsualización pantalla " + aux1 + "/" + aux2);
+
+        //Se obtiene la partida actual a partir del indice que se proporciona
+        PantallaModelo pantalla = (PantallaModelo) partidaModelo.getPantallasPartida().values().toArray()[indice];
+
+        //Se actualizan los parametros correspondientes
+        textInfo.setText(pantalla.getTexto().getTexto());
+        textPreg.setText(pantalla.getQuiz().getPregunta());
+        textOpc1.setText(pantalla.getQuiz().getOpcionA());
+        textOpc2.setText(pantalla.getQuiz().getOpcionB());
+        textOpc3.setText(pantalla.getQuiz().getOpcionC());
+        textOpc4.setText(pantalla.getQuiz().getOpcionD());
+
+        //Se actualizan las opciones para mostrar cual es la correcta
+        if (pantalla.getQuiz().getOpcionA().equals(pantalla.getQuiz().getSolucion())){
+            textOpc1.setText(pantalla.getQuiz().getOpcionA() + " (CORRECTA)");
+        }
+        else if (pantalla.getQuiz().getOpcionB().equals(pantalla.getQuiz().getSolucion())){
+            textOpc2.setText(pantalla.getQuiz().getOpcionB() + " (CORRECTA)");
+        }
+        else if (pantalla.getQuiz().getOpcionC().equals(pantalla.getQuiz().getSolucion())){
+            textOpc3.setText(pantalla.getQuiz().getOpcionC() + " (CORRECTA)");
+        }
+        else {
+            textOpc4.setText(pantalla.getQuiz().getOpcionD() + " (CORRECTA)");
+        }
     }
 }
